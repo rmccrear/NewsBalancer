33943b9acea671d5db42dcad842704dcc628339b
Robert McCreary Mannino <fooyinfighter@gmail.com>
2023-03-20 23:43:37 -0400

    getNews search string

--- lib/getNews.ts
+++ lib/getNews.ts
@@ -1,8 +1,10 @@
 const apiUrl = "https://newssearcherpyapi.rmccrear.repl.co/news"
 
 const getNews = async (searchString: string) => {
-  console.log("get News")
-  const resp = await fetch(apiUrl);
+  // const resp = await fetch(apiUrl);
+  let resp;
+  if(searchString) resp = await fetch(apiUrl + "/" + searchString);
+  else resp = await fetch(apiUrl);
   const news = await resp.json();
   console.log(news)
   return news;
