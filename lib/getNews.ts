c577f9f77086864925e32ff421df9a75012ba626
Robert McCreary Mannino <fooyinfighter@gmail.com>
2023-03-25 22:43:50 -0400

    use .env to config API_URL

--- lib/getNews.ts
+++ lib/getNews.ts
@@ -1,9 +1,9 @@
-const apiUrl = "https://newssearcherpyapi.rmccrear.repl.co/news"
+const apiUrl: string =  process.env.NEXT_PUBLIC_API_SERVER;
 
 const getNews = async (searchString: string) => {
   // const resp = await fetch(apiUrl);
   let resp;
-  if(searchString) resp = await fetch(apiUrl + "/" + searchString);
+  if(searchString) resp = await fetch(apiUrl + "/news/" + searchString);
   else resp = await fetch(apiUrl);
   const news = await resp.json();
   console.log(news)
--- lib/getOpp.ts
+++ lib/getOpp.ts
@@ -1,11 +1,11 @@
 import axios from 'axios';
 import {type Article} from './models';
 
-const apiUrl = "https://newssearcherpyapi.rmccrear.repl.co/opposing_view";
+const apiUrl: string =  process.env.NEXT_PUBLIC_API_SERVER;
 
 export const getOpp = async (name: string, description: string, sentiment: string, url: string) => {
     console.log("url: " + url);
-    const resp = await axios.post<Article[]>(apiUrl, {
+    const resp = await axios.post<Article[]>(apiUrl + "/opposing_view", {
         name,
         description,
         sentiment,
